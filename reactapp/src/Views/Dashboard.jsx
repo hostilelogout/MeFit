3d83517cfc778581b98c83037f85f5df5b7e3dca
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-03-16 14:09:19 +0100

    Fix: corrected some files

--- reactapp/src/Views/Dashboard.jsx
+++ reactapp/src/Views/Dashboard.jsx
@@ -3,8 +3,11 @@ import keycloak from "../keycloak";
 function Dashboard (){
   return (
   <div>
-      <h1>DashBoard</h1>
-
+          <h1>DashBoard</h1>
+          {
+              console.log(keycloak.token)
+          }
+          
     </div>
   );
 }
